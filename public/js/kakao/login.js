17f9a2b19b12201c2b98899f3c20fbd36ce9ed46
skyfly33 <imfly7@naver.com>
2015-07-02 13:18:32 +0900

    custom login button & kaka login script seperate.

--- public/js/kakao/login.js
+++ public/js/kakao/login.js
@@ -1,15 +1,14 @@
 // 사용할 앱의 Javascript 키를 설정해 주세요.
 Kakao.init('db831e72d4093199ddd7954e27a2f91b');
 
-// 카카오 로그인 버튼을 생성합니다.
-Kakao.Auth.createLoginButton({
-    container: '#kakao-login-btn',
-    lang: 'en',
- 	size: 'small',
+function loginWithKakao() {
+	// 카카오 로그인 버튼을 생성합니다.
+	Kakao.Auth.login({
 	    success: function(authObj) {
-    	 	alert(JSON.stringify(authObj));
-    	},
-    	fail: function(err) {
-      		alert(JSON.stringify(err))
-    	}
-});
+		 	alert(JSON.stringify(authObj));
+		},
+		fail: function(err) {
+	  		alert(JSON.stringify(err))
+		}
+	});
+};
